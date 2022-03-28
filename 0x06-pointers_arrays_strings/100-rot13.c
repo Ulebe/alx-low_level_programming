4986eeb535b3fa203e1f49471ec341bc00da1a2d
Ulebe <Onosulebe8@gmail.com>
2022-03-28 08:11:39 -0700

    Write a function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,34 +1,28 @@
 #include "main.h"
 /**
- *rot13 - encodes strings using rot13.
- *@s: pointer to string.
- *
- *Return: pointer to encoded string.
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
  */
+
 char *rot13(char *s)
 {
-	int stringCount, rotation;
-	char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		     'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-		     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-		     'Z'};
-	char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		     'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-		     'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-		     'M'};
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	for (stringCount = 0; s[stringCount] != '\0'; stringCount++)
+	while (*(s + count) != '\0')
 	{
-		for (rotation = 0; rotation < 53; rotation++)
+		for (i = 0; i < 52; i++)
 		{
-			if (r1[rotation] == s[stringCount])
+			if (*(s + count) == alphabet[i])
 			{
-				s[stringCount] = r2[rotation];
+				*(s + count) = rot13[i];
 				break;
 			}
 		}
+		count++;
 	}
+
 	return (s);
 }
